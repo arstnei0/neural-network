afb64eb2621e7b7e082294771dd1dfa3add6f541
Zihan Chen <zccmczh@gmail.com>
2023-03-07 21:37:55 +0800

    Update

--- src/lib/page.ts
+++ src/lib/page.ts
@@ -1,57 +1,55 @@
-type Point = {
-	x: number
-	y: number
-}
+import { Perceptron, Point } from "./perceptron"
 
-const canvasScale = {
-	x: 10,
-	y: 10,
-}
-
-const fn = (point: Point): boolean => point.x > point.y
-
-export namespace Point {
-	export const draw = (canvas: CanvasRenderingContext2D, point: Point) => {
-		const color = fn(point) ? "green" : "red"
-		canvas.fillStyle = color
-		canvas.beginPath()
-		canvas.arc(point.x * canvasScale.x, point.y * canvasScale.y, 5, 0, 2 * Math.PI)
-		canvas.fill()
-	}
-}
+const fn = (x: number): number => x * 2.5
+const evaluate = (point: Point): number => (fn(point.x) < point.y ? 1 : -1)
+const scale = 5
 
 const random = () => Math.random() * 100
 
-const points = Array.from({ length: 1000 }, () => ({ x: random(), y: random() } as Point))
+const points = Array.from({ length: 2000 }, () => Point.random())
 
-const canvasEl = document.createElement("canvas")
-canvasEl.width = 100 * canvasScale.x
-canvasEl.height = 100 * canvasScale.y
-document.body.appendChild(canvasEl)
-
-const canvas = canvasEl.getContext("2d") as CanvasRenderingContext2D
-
-points.map((p) => Point.draw(canvas, p))
-
-type Perceptron = { weights: [number, number] }
-const perceptron: Perceptron = {
-	weights: [1, 1],
+const drawLine = (canvas: CanvasRenderingContext2D, p1: Point, p2: Point) => {
+	canvas.beginPath()
+	canvas.moveTo(p1.x * scale, p1.y * scale)
+	canvas.lineTo(p2.x * scale, p2.y * scale)
+	canvas.stroke()
 }
 
-const sign = (n: number): boolean => n < 0
-
-let learningRate = 0.1
-
-const update = (perceptron: Perceptron, point: Point) => {
-	const guess = sign(perceptron.weights[0] * point.x + perceptron.weights[1] * point.y)
-	const guessN = guess ? 1 : -1
-	const expected = fn(point)
-	const expectedN = expected ? 1 : -1
-	const error = expectedN - guessN
-	perceptron.weights[0] += error * point.x * learningRate
-	perceptron.weights[1] += error * point.y * learningRate
-	console.log(perceptron.weights[0], perceptron.weights[1])
+const displayFn = (canvas: CanvasRenderingContext2D, fn: (x: number) => number) => {
+	const pointOne = new Point(0, fn(0))
+	const pointTwo = new Point(100, fn(100))
+	drawLine(canvas, pointOne, pointTwo)
 }
 
-const results = points.map((point) => update(perceptron, point))
-console.log(results)
+{
+	const canvasEl = document.createElement("canvas")
+	canvasEl.width = 100 * scale
+	canvasEl.height = 100 * scale
+	document.body.appendChild(canvasEl)
+
+	const canvas = canvasEl.getContext("2d") as CanvasRenderingContext2D
+	displayFn(canvas, fn)
+	points.map((p) => p.draw(canvas, scale, evaluate(p)))
+}
+
+{
+	const canvasEl = document.createElement("canvas")
+	canvasEl.width = 100 * scale
+	canvasEl.height = 100 * scale
+	document.body.appendChild(canvasEl)
+
+	const canvas = canvasEl.getContext("2d") as CanvasRenderingContext2D
+	const perceptron = new Perceptron()
+
+	function timeout(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms))
+	}
+
+	for (const p of points) {
+		const guess = perceptron.guess(p)
+		const error = perceptron.learn(p, guess, evaluate(p))
+		displayFn(canvas, perceptron.fn.bind(perceptron))
+		p.draw(canvas, scale, error === 0 ? 1 : -1)
+		await timeout(1)
+	}
+}
--- src/lib/perceptron.ts
+++ src/lib/perceptron.ts
@@ -1,15 +1,62 @@
 import type { Tuple } from "./utils/types"
 
-export type Perceptron<Inputs extends number = 2> = {
-	weights: Tuple<number, Inputs>
-	bias: number
+// export type Perceptron<Inputs extends number = 2> = {
+// 	weights: Tuple<number, Inputs>
+// 	bias: number
+// }
+
+// export namespace Perceptron {
+// 	export const create = <Inputs extends number = 2>(inputs: Inputs): Perceptron<Inputs> => {
+// 		return {
+// 			weights: Array.from({ length: inputs }, () => 1) as Perceptron<Inputs>["weights"],
+// 			bias: 0,
+// 		}
+// 	}
+// }
+
+export class Point {
+	x: number
+	y: number
+
+	constructor(x: number, y: number) {
+		this.x = x
+		this.y = y
+	}
+
+	static random() {
+		return new Point(Math.random() * 100, Math.random() * 100)
+	}
+
+	draw(canvas: CanvasRenderingContext2D, scale: number = 5, guess: number) {
+		canvas.fillStyle = guess === -1 ? "red" : "green"
+		canvas.beginPath()
+		canvas.arc(this.x * scale, this.y * scale, 5, 0, 2 * Math.PI)
+		canvas.fill()
+	}
 }
 
-export namespace Perceptron {
-	export const create = <Inputs extends number = 2>(inputs: Inputs): Perceptron<Inputs> => {
-		return {
-			weights: Array.from({ length: inputs }, () => 1) as Perceptron<Inputs>["weights"],
-			bias: 0,
-		}
+export class Perceptron {
+	weights = [1, 1]
+	learningRate = 0.01
+	constructor() {}
+
+	sign(n: number): number {
+		return n < 0 ? -1 : 1
+	}
+
+	guess(p: Point) {
+		const sum = this.weights[0] * p.x + this.weights[1] * p.y
+		return this.sign(sum)
+	}
+
+	learn(p: Point, guess: number, expected: number) {
+		const error = expected - guess
+		this.weights[0] += error * p.x * this.learningRate
+		this.weights[1] += error * p.y * this.learningRate
+		return error
+	}
+
+	fn(x: number) {
+		return -x * (this.weights[0] / this.weights[1])
 	}
 }
